cbee4d96d7e95395b2f78837e555c0a1cae569b3
mhpenta <cogito.huckelberry@gmail.com>
2025-01-25 14:28:51 -0700

    *

--- jsonext/errors.go
+++ jsonext/errors.go
@@ -1,1 +1,48 @@
 package jsonext
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"strings"
+)
+
+func IsUnmarshallingError(err error) bool {
+	if err == nil {
+		return false
+	}
+
+	var syntaxErr *json.SyntaxError
+	if errors.As(err, &syntaxErr) {
+		return true
+	}
+
+	var unmarshalTypeErr *json.UnmarshalTypeError
+	if errors.As(err, &unmarshalTypeErr) {
+		return true
+	}
+
+	var invalidUnmarshalErr *json.InvalidUnmarshalError
+	if errors.As(err, &invalidUnmarshalErr) {
+		return true
+	}
+
+	if errors.Is(err, io.ErrUnexpectedEOF) {
+		return true
+	}
+
+	errStr := err.Error()
+	commonErrors := []string{
+		"invalid character",
+		"cannot unmarshal",
+		"unexpected end of JSON input",
+	}
+
+	for _, phrase := range commonErrors {
+		if strings.Contains(errStr, phrase) {
+			return true
+		}
+	}
+
+	return false
+}
